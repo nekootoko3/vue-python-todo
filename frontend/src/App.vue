ec73c30a3968a58859f242e3b9c98798105e1c12
nekootoko3 <miyako.kerria@gmail.com>
2020-08-30 15:26:06 +0900

    Display todo list mock

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Todo App</h1>
+    <ul>
+      <TodoListItem
+        v-for="todoItem in todoItems"
+        :key="todoItem.id"
+        v-bind:title="todoItem.title"
+      />
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import TodoListItem from './components/TodoListItem.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    TodoListItem,
   },
 })
-export default class App extends Vue {}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+export default class App extends Vue {
+  todoItems: Array<{id: number; title: string}> = [
+    { id: 1, title: 'neko' },
+    { id: 2, title: 'inu' },
+  ];
 }
-</style>
+</script>
---
+++ frontend/src/components/TodoListItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <li>{{ title }}</li>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TodoListItem extends Vue {
+  @Prop() private title!: string;
+}
+</script>
