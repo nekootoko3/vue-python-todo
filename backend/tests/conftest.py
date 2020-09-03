edf0bfa271fa30a99fcd1a0b7857601bdb1fa716
nekootoko3 <miyako.kerria@gmail.com>
2020-09-03 22:16:23 +0900

    Add tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,3 +1,67 @@
-import pytest
+import os
+from typing import Optional, AsyncIterable
 
-# TODO: create fixture
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+from sqlalchemy_utils import create_database, database_exists, drop_database
+from fastapi.testclient import TestClient
+
+from app.app.main import app
+from app.app.database import Base, get_db
+
+TEST_DATABASE_URL = os.environ["TEST_DATABASE_URL"]
+engine = create_engine(TEST_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_test_db() -> AsyncIterable[Session]:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def create_test_database():
+    """
+  Create a clean database on every test case.
+
+  We use the `sqlalchemy_utils` package here for a few helpers in consistently
+  creating and dropping the database.
+  """
+    if database_exists(TEST_DATABASE_URL):
+        drop_database(TEST_DATABASE_URL)
+    create_database(TEST_DATABASE_URL)  # Create the test database.
+    Base.metadata.create_all(bind=engine)  # Create the tables.
+    app.dependency_overrides[get_db] = get_test_db  # Mock the Database Dependency
+    yield  # Run the tests.
+    drop_database(TEST_DATABASE_URL)  # Drop the test database.
+
+
+@pytest.yield_fixture
+def db():
+    """Returns an sqlalchemy session, and after the test tears down everything properly."""
+
+    session = Session(bind=engine)
+
+    yield session
+    # Drop all data after each test
+    for tbl in reversed(Base.metadata.sorted_tables):
+        engine.execute(tbl.delete())
+    # put back the connection to the connection pool
+    session.close()
+
+
+@pytest.fixture()
+def client():
+    """
+    When using the 'client' fixture in test cases, we'll get full database
+    rollbacks between test cases:
+    """
+    with TestClient(app) as client:
+        yield client
--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,17 +1,35 @@
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
 
-from app.main import app
+from app.app import crud, schemas
 
-client = TestClient(app)
-
-
-def test_read_item_empty():
+def test_read_item_empty(client: TestClient):
     response = client.get("/api/v1/todos")
     assert response.status_code == 200
+    assert response.json() == []
 
-def test_create_todo():
+def test_read_items(client: TestClient, db: Session):
+    title = "todo0"
+    model_todo = crud.create_todo(db, schemas.TodoCreate(title=title))
+    response = client.get("/api/v1/todos")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+    res_todo = response.json()[0]
+    assert res_todo["id"] == model_todo.id
+    assert res_todo["title"] == model_todo.title
+
+def test_create_todo(client: TestClient):
     response = client.post(
         "/api/v1/todos",
         json={"title": "todo0"}
     )
     assert response.status_code == 201
+
+def test_update_todo(client: TestClient, db: Session):
+    title = "todo0"
+    model_todo = crud.create_todo(db, schemas.TodoCreate(title=title))
+    title_updated = "updated_todo"
+    response = client.put(f"/api/v1/todos/{model_todo.id}", json={"title": title_updated})
+    assert response.status_code == 200
+    res_todo = response.json()
+    assert res_todo["title"] == title_updated
